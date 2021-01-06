2acf7b8eb26f4f69c5c12d113761f723afc9df6d
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-06 18:46:40 +0000

    Updated comments

--- cuttingsticks/src/main/java/cuttingsticks/CuttingSticks.java
+++ cuttingsticks/src/main/java/cuttingsticks/CuttingSticks.java
@@ -2,6 +2,8 @@ package cuttingsticks;
 
 class CuttingStick {
 
+  private static int counter = 0;
+
   public static int getMinCost(int[] arr, int length) {
     for (int i : arr) {
       if (i > length)
@@ -20,13 +22,17 @@ class CuttingStick {
   // * @endIdx - Ending index between cutting points
   // *****************************************************
   private static int getMinCost(int[] arr, int startLength, int endLength, int startIdx, int endIdx) {
+    // System.out.println("Stick length: " + startLength + ", " + endLength);
+    // System.out.println("Array index: " + startIdx + ", " + endIdx);
+    // counter++;
+    // System.out.println("------------");
     // Wood cannot be further divided
     if (startIdx > endIdx) return 0;
     // Find current Cost - Length of stick
     int currentCost = endLength - startLength;
     // Temp variable -Initialize the additional cost iwth MAX int value
     int minCost = Integer.MAX_VALUE;
-    // Find the cuting point such that cost will be minimum recursively
+    // Check different cuting point such that cost will be minimum recursively
     for (int i = startIdx; i <= endIdx; i++) {
       int currentMinCost = getMinCost(arr, startLength, arr[i], startIdx, i - 1)
           + getMinCost(arr, arr[i], endLength, i + 1, endIdx);
@@ -39,9 +45,10 @@ class CuttingStick {
   }
 
   public static void main(String[] args) {
-    int arr[] = { 2, 4, 7, 12 };
-    int length = 20;
+    int arr[] = { 2, 4, 5, 6, 7 };
+    int length = 10;
     int minCost = getMinCost(arr, length);
     System.out.println("Min Cost : " + minCost);
+    // System.out.println(counter+"");
   }
 }
