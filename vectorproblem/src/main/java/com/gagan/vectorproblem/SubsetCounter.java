720f773445dabd6634df6628444b1d749fa24c94
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-06 20:27:48 +0000

    Added subset counter

--- vectorproblem/src/main/java/com/gagan/vectorproblem/SubsetCounter.java
+++ vectorproblem/src/main/java/com/gagan/vectorproblem/SubsetCounter.java
@@ -6,29 +6,32 @@ import java.util.Arrays;
  * Hello world!
  *
  */
-public class App {
+public class SubsetCounter {
+
 
     // Function that return the
     // count of subset such that
     // Math.min(S) + Math.max(S) < K
-    static int getSubsetCount(int arr[], int K, int N) {
+    public static int getSubsetCount(int arr[], int K) {
 
         // Sorting the array
         Arrays.sort(arr);
 
         int left, right;
         left = 0;
-        right = N - 1;
+        right = arr.length - 1;
 
         // ans stores total number
         // of subsets
         int ans = 0;
 
         while (left <= right) {
-            if (arr[left] + arr[right] < K) {
+            if (arr[left] + arr[right] <= K) {
 
                 // Add all posible subsets
                 // between i and j
+                // ans += Math.pow(2, right - left);
+                // Two Pointer Technique to count the number of subsets
                 ans += 1 << (right - left);
                 left++;
             } else {
@@ -42,9 +45,8 @@ public class App {
 
     // Driver code
     public static void main(String[] args) {
-        int arr[] = { 2, 4, 5, 7 };
-        int K = 8;
-        int N = arr.length;
-        System.out.print(getSubsetCount(arr, K, N));
+        int arr[] = {2, 4, 5, 7};
+        int K =8;
+        System.out.println("Total subsets: " + getSubsetCount(arr, K));
     }
 }
